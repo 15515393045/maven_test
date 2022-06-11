cc136f9c8f1345b0ddd1f2a7d53539063203e62c
zhanyh <1925815291@qq.com>
2022-06-11 21:21:40 +0800

    hot-fix commit

--- src/main/java/com/atguigu/java/test/Demo1.java
+++ src/main/java/com/atguigu/java/test/Demo1.java
@@ -3,7 +3,10 @@ package com.atguigu.java.test;
 public class Demo1 {
 
     public static void main(String[] args) {
-        System.out.println("hahhahah");
+        System.out.println("hello git 1");
+        System.out.println("hello git 2");
+        System.out.println("hello git 3");
+        System.out.println("hot-fix test");
     }
 
 }
